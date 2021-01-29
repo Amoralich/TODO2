0413a15c6d788ba7b44ca06aa324f3f4d397ae75
Opilane TTHK <opilane@tthk.ee>
2021-01-29 12:39:36 +0200

    rabota

---
+++ controllers/takscontrollers.js
@@ -0,0 +1,26 @@
+const date = require('../getDate.js');
+const Task = require('../models/tasks');
+
+exports.getMainPage = (req, res)=> {
+    Task.fetchTasks(tasks => {
+        let today = {
+        date: date.getDate(),
+        weekday:  date.getWeekDay()
+        };
+
+        console.log(tasks);
+        res.render('index.ejs', {date: today, toDoItems: tasks});
+    });
+};
+
+exports.postnewTask = (req,res) => {
+    let item = new Task(req.body.newTask);
+    item.saveTask();
+    res.redirect('/');
+}
+
+exports.deleteTask = (req,res) => {
+    console.log("Call from delete", req.body.checkbox);
+    Task.deleteTask(req.body.checkbox);
+    res.redirect('/');
+}
